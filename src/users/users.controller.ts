f7aba908ed022b69a69511222c852d1e22fa8a95
Divine Charles <officialkb17@gmail.com>
2024-10-27 09:00:05 +0100

    Inject users service into users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,9 +15,15 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    // Injecting Users Service
+    private readonly usersService: UsersService,
+  ) {}
+
   @Get('/:id?')
   public getUsers(
     @Param() getUserParamDto: GetUsersParamDto,
