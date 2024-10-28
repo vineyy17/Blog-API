3048c0144173560ec3f58c00dce45308d47db358
Divine Charles <officialkb17@gmail.com>
2024-10-28 14:54:37 +0100

    Create POST route in posts controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CreatePostDto } from './dtos/create-post.dto';
 
 @Controller('posts')
 @ApiTags('Posts')
@@ -14,4 +15,7 @@ export class PostsController {
   public getPosts(@Param('userId') userId: string) {
     return this.postsService.findAll(userId);
   }
+
+  @Post()
+  public createPost(@Body() createPostDto: CreatePostDto) {}
 }
