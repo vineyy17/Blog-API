785755db62623c5353437ed7414d4f7369be7843
Divine Charles <officialkb17@gmail.com>
2024-10-28 12:20:22 +0100

    Create swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Use validation pipes globally
+   */
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +16,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  /**
+   * Swagger configuration
+   */
+  const config = new DocumentBuilder().setVersion('1.0').build();
+  // Instantiate Document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
