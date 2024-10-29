82ea797e4aa0d3f5e80b1a8d9a35429c73ea06a5
Divine Charles <officialkb17@gmail.com>
2024-10-29 19:49:30 +0100

    Expand User entity definition

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -23,11 +23,13 @@ export class CreateUserDto {
 
   @IsEmail()
   @IsNotEmpty()
+  @MaxLength(96)
   email: string;
 
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(96)
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
     message:
       'Password must have a minimum of eight characters, including at least one letter, one number and one special character.',
